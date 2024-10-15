705c7c9a5a6533d04f6eb2a6ff9d0d06b70df0b6
Codaerus <christianqc91@gmail.com>
2024-10-15 18:23:53 -0500

    Create code2_sd.ino

---
+++ CLASES/CLASE2/code2_sd.ino
@@ -0,0 +1,36 @@
+#include "FS.h"
+#include "SD_MMC.h"
+
+void setup() {
+  Serial.begin(9600);
+  //incializa la SD
+  if(!SD_MMC.begin())
+    {
+      Serial.println("Error al inicial la SD!");
+      return;
+    }
+    Serial.println("Tarjeta SD inicializada");  
+    //Abrimos el archivo para escribir
+    File file = SD_MMC.open("/datos.txt",FILE_WRITE);
+    if(!file)
+      {
+        Serial.println("Error a labrir el archivo para escribir");
+        return;
+      }
+    //Escribe "Hola en el archivo"
+    if(file.print("Hola"))
+      {
+        Serial.println("Escrito en el archivo: Hola");
+      }
+    else{
+      Serial.println("Error al escribir en el archivo!");
+    }
+    //Cierre el archivo
+    file.close();
+    Serial.println("Archivo cerrado.");
+}
+
+void loop() {
+
+
+}
