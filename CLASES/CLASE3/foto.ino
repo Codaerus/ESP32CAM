2ee695d9abe34d6bc4e5c99b1af2a0554abf7169
Codaerus <christianqc91@gmail.com>
2024-10-17 19:45:34 -0500

    Update foto.ino

--- CLASES/CLASE3/foto.ino
+++ CLASES/CLASE3/foto.ino
@@ -1,96 +1,63 @@
-// Librerías para la cámara
 #include "esp_camera.h"
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
 #include "driver/rtc_io.h"
-// Librerías para la tarjeta MicroSD
+//Librerías para la tarjeta MicroSD
 #include "FS.h"
 #include "SD_MMC.h"
-// Librerías para la memoria EEPROM 
+//Librería para la EEPROM
 #include "EEPROM.h"
-//Pinout cámara
+//Incluimos pinout
 #include "pinout.h"
-// Utilice 1 byte de espacio EEPROM
+//Utilizamos 1 bytes para la EEPROM
 #define EEPROM_SIZE 1
-// Contador de número de imagen
+//Contador de número de imagen
 unsigned int contadorFoto = 0;
 
-
-void initMicroSDCard() {
-  // Inicia la tarjeta MicroSD
-  Serial.println("Montaje de la tarjeta MicroSD");
-  if (!SD_MMC.begin()) {
-    Serial.println("Error al montar la tarjeta MicroSD");
+void takeNewPhoto(String path){
+  //Tomar una foto con la cámara
+  camera_fb_t *fb = esp_camera_fb_get(); //captura una imagen (frame buffer)
+  if(!fb){
+    Serial.println("Fallo la captura");
     return;
   }
-  uint8_t cardType = SD_MMC.cardType();
-  if (cardType == CARD_NONE) {
-    Serial.println("No se encontró ninguna tarjeta MicroSD");
-    return;
-  }
-}
-
-void takeNewPhoto(String path) {
-  // Tomar una foto con la cámara
-  camera_fb_t * fb = esp_camera_fb_get();
-  if (!fb) {
-    Serial.println("Falló la captura de la cámara");
-    return;
-  }
-
-  // Guardar imagen en la tarjeta microSD
+  //Guardar la imagen en la tarjeta microSD
   fs::FS &fs = SD_MMC;
   File file = fs.open(path.c_str(), FILE_WRITE);
-  if (!file) {
-    Serial.println("No se pudo abrir el archivo en modo de escritura");
-  } else {
-    file.write(fb->buf, fb->len); // payload (imagen), longitud del payload
-    Serial.printf("Archivo guardado en la ruta: %s\n", path.c_str());
+  if (!file){
+    Serial.println("No se pudo cear el archivo");
+  }else{
+    file.write(fb->buf,fb->len); //payload(imagen), longitud del payload
+    Serial.printf("Archivo guardado en la ruta:%s\n",path.c_str());
   }
-
-  // Cerrar el archivo
+  //cerrar el archivo
   file.close();
-  // Devolver el frame buffer al controlador para su reutilización
-  esp_camera_fb_return(fb);
+  esp_camera_fb_return(fb);//Liberar recursos
 }
-
 void setup() {
-  // Desactivar el detector de caída de tensión
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
-  // Iniciar monitor serie
   Serial.begin(115200);
-
-  // Inicializa la cámara
-  Serial.print("Inicializando el módulo de la cámara...");
+  //Desactivamos el detector de caída de tensión para evitar reinicios inesperados
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG,0);
+  //Inicializa la cámara
+  Serial.println("Iniciando el módulo de la camera...");
   configuracionCamara();
-  Serial.println("Camera OK!");
-
-  // Inicializa la MicroSD
-  Serial.print("Inicializando el módulo de la tarjeta MicroSD... ");
+  Serial.println("camera OK!");
+  //Iniciamos la MicroSD
+  Serial.println("Iniciando el módulo de la tarjeta MicroSD...");
   initMicroSDCard();
-
-  // Inicializar EEPROM con tamaño predefinido
+  //Iniciamos EEPROM con tamaño definido
   EEPROM.begin(EEPROM_SIZE);
   contadorFoto = EEPROM.read(0) + 1;
+  String path = "/foto" + String(contadorFoto)+ ".jpg";
 
-  // Ruta donde se guardará la nueva imagen en la tarjeta SD
-  String path = "/foto" + String(contadorFoto) + ".jpg";
-  Serial.printf("Nombre del archivo de imagen: %s\n", path.c_str());
-
-  // Tomar y guardar foto
+  //tomar y guardar foto
   takeNewPhoto(path);
-
-  // Actualizar el contador de números de imagen en EEPROM
-  EEPROM.write(0, contadorFoto);
-  EEPROM.commit();
-
-  // Esperar un momento antes de entrar en modo de suspensión
+  //Actualizar el contador de números de imagen en EEPROM
+  EEPROM.write(0,contadorFoto); //Escribe un nuevo valror en la dirección 0 de la EEPROM
+  EEPROM.commit();//Guarda los cambios
+  //Esperar un momento antes de entrar al modo suspensión
   delay(1000);
-  
-  // Ingrese al modo de sueño profundo
   esp_deep_sleep_start();
 }
 
 void loop() {
-  // No se necesita código en el loop
+
 }
