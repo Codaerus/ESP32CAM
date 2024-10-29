109364519917903d8fbbae9ebf68b0dfc93d1b4c
Codaerus <christianqc91@gmail.com>
2024-10-29 17:51:21 -0500

    Create wifi_code.ino

---
+++ CLASES/CLASE6/wifi_code.ino
@@ -0,0 +1,21 @@
+#include <WiFi.h>
+
+void setup() {
+  setup_wifi();
+}
+
+void loop() {
+
+
+}
+
+void setup_wifi(){
+  Serial.begin(9600);
+  WiFi.begin("MOVISTAR_78A8","NS2ajtQJ7TtDt9m");//DHCP
+  while(WiFi.status() != WL_CONNECTED)
+    {
+      Serial.println(".");
+      delay(300);
+    }
+  Serial.println(WiFi.localIP());
+}
