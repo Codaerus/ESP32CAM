ef5974feded09fc475e30fc541562d92e0b5f5b6
Codaerus <christianqc91@gmail.com>
2024-11-12 18:15:36 -0500

    Create envio_http_apache.ino

---
+++ CLASES/CLASE8/envio_http_apache.ino
@@ -0,0 +1,55 @@
+#include "esp_camera.h"
+#include <esp_wifi.h>
+#include <HTTPClient.h>
+#include <Base64.h>
+//Incluimos pinout
+#include "pinout.h"
+const char *serverURL = "http://192.168.1.5/dashboard/DATOS/upload.php";
+void captureSend(){
+  //Tomar una foto con la cámara
+  camera_fb_t *fb = esp_camera_fb_get(); //captura una imagen (frame buffer)
+  if(!fb){
+    Serial.println("Fallo la captura");
+    return;
+  }
+  /*************************  BASE 64  ****************************/
+  String imageBase64 = "data:image/jpeg;base64," + base64::encode((const uint8_t *)fb->buf,fb->len);
+  /**************************  HTTP  ******************************/
+  HTTPClient http;
+  http.begin(serverURL);
+  http.addHeader("Content-Type", "application/json");
+  //Creamos un String JSON
+  String jsonPayload = "{imageData:" + imageBase64 + "}";
+  int code = http.POST(jsonPayload);
+  if (code > 0){
+    String response = http.getString();
+    Serial.println(response);
+  }
+  http.end();
+  /****************************************************************/
+  esp_camera_fb_return(fb);//Liberar recursos
+}
+
+void setup() {
+  setup_wifi();
+  //Inicializa la cámara
+  Serial.println("Iniciando el módulo de la camera...");
+  configuracionCamara();
+  Serial.println("camera OK!");
+}
+
+void loop() {
+  delay(15000);
+  captureSend();
+}
+
+void setup_wifi(){
+  Serial.begin(9600);
+  WiFi.begin("MOVISTAR_78A8","NS2ajtQJ7TtDt9m");//DHCP
+  while(WiFi.status() != WL_CONNECTED)
+    {
+      Serial.println(".");
+      delay(300);
+    }
+  Serial.println(WiFi.localIP());
+}
